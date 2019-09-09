82ea27cade75dd31b9cf4f784cfc24023936c383
MOSES <MOSESLAZARO2019@outlook.com>
2019-09-09 19:57:19 +0300

    Updates

---
+++ precache-manifest.0f6553ceee8babbf290e84352daca3d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4826c6b035158af755b89a9206154cff",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "d556bf69f59996f43579",
+    "url": "/todo-app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "55c5ffbdf9b444f881d9",
+    "url": "/todo-app/static/js/2.0e4c32cc.chunk.js"
+  },
+  {
+    "revision": "d556bf69f59996f43579",
+    "url": "/todo-app/static/js/main.2b84fc82.chunk.js"
+  },
+  {
+    "revision": "bfcef0091b5f9486490c",
+    "url": "/todo-app/static/js/runtime~main.0e041480.js"
+  }
+]);
